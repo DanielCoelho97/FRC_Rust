4788b33fbd1e7846c59cff433ae94478244f8b96
DanielCoelho97 <37227652+DanielCoelho97@users.noreply.github.com>
2019-03-26 15:50:52 -0300

    Add files via upload

--- src/EventoPeca.rs
+++ src/EventoPeca.rs
@@ -2,7 +2,7 @@ struct EventoPeca;
 
 impl EventoPeca {
     let mut local: String;
-    let duracao: f64;
+    let mut duracao: f64;
     let mut pessoas: Vec<Pessoa> = Vec::new();
     let textoPeca: TextoPeca; 
 }
